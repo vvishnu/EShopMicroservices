8223999067f96dd9768d30ca8ae54d5c5ed406ad
Vishnu Vishweshwar <VISH396534@exactsoftware.com>
2024-10-23 21:50:34 +0530

    Added Crud operations

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿
+using Catalog.API.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler
+        (IDocumentSession documentSession, ILogger<DeleteProductCommandHandler> logger)
+        : ICommandHandler<DeleteProductCommand,DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"DeleteProductCommandHandler.Handle called with {command}");
+
+            documentSession.Delete<Product>(command.Id);
+
+            await documentSession.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿
+using MediatR;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var request = new DeleteProductRequest(id);
+
+                var result = await sender.Send(request.Adapt<DeleteProductCommand>());
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithDescription("Delete Product")
+            .WithSummary("Delete Product");
+        }
+    }
+}
